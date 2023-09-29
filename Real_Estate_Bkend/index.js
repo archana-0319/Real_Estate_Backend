c930f3e95a3c0b53538688507cbde833fb88d61e
Archana <archanasreenivas.k@gmail.com>
2023-09-29 14:52:56 +0530

    commited

---
+++ Real_Estate_Bkend/index.js
@@ -0,0 +1,27 @@
+
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const propertyRouter = require("./routes/property");
+const cors = require("cors");
+const userRouter = require("./routes/user");
+require('dotenv').config();
+const app = express();
+
+app.use(express.static("uploads"));
+app.use(cors({}));
+
+app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+
+mongoose.connect(process.env.MONGO_DB).then(res=>{
+    console.log("Connected to Database Successfully");
+}).catch(err=>{
+    console.log("Connection failed");
+})
+
+let port = process.env.PORT;
+
+app.use("/prop",propertyRouter);
+app.use("/user",userRouter);
+app.listen(port, () => console.log(`app is running on port ${port}`));
